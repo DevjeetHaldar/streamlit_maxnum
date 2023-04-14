119e83f7232546dd9b466476f721e61c3e328f77
DevjeetHaldar <127197144+DevjeetHaldar@users.noreply.github.com>
2023-04-14 20:11:40 +0530

    Update max_num.py

--- max_num.py
+++ max_num.py
@@ -1,12 +1,7 @@
 import streamlit as st
 
 def find_largest(a, b, c):
-    if a >= b and a >= c:
-        return a
-    elif b >= a and b >= c:
-        return b
-    else:
-        return c
+    return max(a, b, c)
 
 st.title("Find the Largest Number")
 
