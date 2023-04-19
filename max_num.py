bd39889bb81b1217acea610ac8e60545d7b4dc85
DevjeetHaldar <127197144+DevjeetHaldar@users.noreply.github.com>
2023-04-19 19:57:43 +0530

    Update max_num.py

--- max_num.py
+++ max_num.py
@@ -4,6 +4,7 @@ def find_largest(a, b, c):
     return max(a, b, c)
 
 st.title("Find the Largest Number")
+st.write("Made by Devjeet Haldar")
 
 a = st.number_input("Enter the first number:")
 b = st.number_input("Enter the second number:")
